2de981dea22b25f6dbf88c19717eff6e3b33cdea
HueWoon Kim <kho0307@gmail.com>
2021-10-05 22:13:59 +0900

    feat: 숫자 공의 기능을 가진 Ball 클래스 추가

---
+++ src/main/java/com/nextstep/baseball/Ball.java
@@ -0,0 +1,33 @@
+package com.nextstep.baseball;
+
+public class Ball {
+	private final int number;
+	private final int position;
+
+	public Ball(int number, int position) {
+		if (number < 1 || number > 9) {
+			throw new IllegalArgumentException(number + "가 1 ~ 9까지의 범위를 벗어났습니다.");
+		}
+		if (position < 1 || position > 3) {
+			throw new IllegalArgumentException(position + "가 1 ~ 3까지의 위치를 벗어났습니다.");
+		}
+		this.number = number;
+		this.position = position;
+	}
+
+	public boolean matchValue(Ball other) {
+		return other.matchValue(this.number);
+	}
+
+	public boolean matchValue(int number) {
+		return this.number == number;
+	}
+
+	public boolean matchPosition(Ball other) {
+		return other.matchPosition(this.position);
+	}
+
+	public boolean matchPosition(int position) {
+		return this.position == position;
+	}
+}
---
+++ src/test/java/com/nextstep/baseball/BallTest.java
@@ -0,0 +1,38 @@
+package com.nextstep.baseball;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BallTest {
+	@Test
+	@DisplayName("숫자의 범위는 1 ~ 9")
+	void numberScope() {
+		assertThatThrownBy(() -> new Ball(0, 1)).isExactlyInstanceOf(IllegalArgumentException.class);
+		assertThatThrownBy(() -> new Ball(10, 1)).isExactlyInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	@DisplayName("위치의 범위는 1 ~ 3")
+	void positionScope() {
+		assertThatThrownBy(() -> new Ball(1, 0)).isExactlyInstanceOf(IllegalArgumentException.class);
+		assertThatThrownBy(() -> new Ball(9, 4)).isExactlyInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	@DisplayName("같은 값인지 확인")
+	void matchValue() {
+		Ball ball = new Ball(2, 1);
+		assertThat(ball.matchValue(new Ball(1, 1))).isFalse();
+		assertThat(ball.matchValue(new Ball(2, 1))).isTrue();
+	}
+
+	@Test
+	@DisplayName("같은 위치인지 확인")
+	void matchPositon() {
+		Ball ball = new Ball(2, 1);
+		assertThat(ball.matchPosition(new Ball(2, 2))).isFalse();
+		assertThat(ball.matchPosition(new Ball(2, 1))).isTrue();
+	}
+}
