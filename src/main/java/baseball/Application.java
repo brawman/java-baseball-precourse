c46d3e1c3e363e26a4a78a37256f3bb386a5474d
HueWoon Kim <kho0307@gmail.com>
2021-10-06 20:43:57 +0900

    refactor: 사용자 입력에 대한 예외처리 부분 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,17 +5,20 @@ import baseball.domain.InningResult;
 import baseball.ui.BaseballUI;
 
 public class Application {
+	private static void playGameWithExceptionHandler() {
+		try {
+			InningResult result = BaseballGame.play(BaseballUI.userGameStrategy());
+			BaseballUI.drawGameResult(result);
+		} catch(IllegalArgumentException exception) {
+			System.err.println("[ERROR]" + exception.getMessage());
+		}
+	}
+
 	public static void main(String[] args) {
 		BaseballGame.ready();
 		while (!BaseballGame.isEnd()
 			|| BaseballGame.stopOrRestart(BaseballUI.isGameContinue()) == BaseballGame.RESTART) {
-			try {
-				InningResult result = BaseballGame.play(BaseballUI.userGameStrategy());
-				BaseballUI.drawGameResult(result);
-
-			} catch(IllegalArgumentException exception) {
-				System.err.println("[ERROR]" + exception.getMessage());
-			}
+			playGameWithExceptionHandler();
 		}
 	}
 }
